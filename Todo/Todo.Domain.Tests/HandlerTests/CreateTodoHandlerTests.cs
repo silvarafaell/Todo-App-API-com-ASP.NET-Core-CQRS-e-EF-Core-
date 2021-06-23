1106b37d862b800c5e9e8e904b94d3c1632f5c28
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-06-23 13:31:50 -0300

    Fake Repositories

--- Todo/Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
+++ Todo/Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Todo.Domain.Commands;
 using Todo.Domain.Handlers;
+using Todo.Domain.Tests.Repositories;
 
 namespace Todo.Domain.Tests.HandlerTests
 {
@@ -12,7 +13,7 @@ namespace Todo.Domain.Tests.HandlerTests
         public void Dado_comando_invalido_deve_interromper_a_execucao()
         {
             var command = new CreateTodoCommand("", "", DateTime.Now);
-            var handler = new TodoHandler(null);
+            var handler = new TodoHandler(new FakeTodoRepository());
             Assert.Fail();
         }
 
