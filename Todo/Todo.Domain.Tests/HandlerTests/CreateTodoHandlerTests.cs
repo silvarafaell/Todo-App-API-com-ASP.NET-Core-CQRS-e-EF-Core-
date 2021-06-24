0c114485a55bc789f6fe795d2a4b6b7679ecf571
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-06-24 08:36:44 -0300

    Testando o Handler

--- Todo/Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
+++ Todo/Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -7,20 +7,30 @@ using Todo.Domain.Tests.Repositories;
 namespace Todo.Domain.Tests.HandlerTests
 {
     [TestClass]
-    public class CreateTodoCommandHandlerTests
+    public class CreateTodoHandlerTests
     {
+        private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.Now);
+        private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Titulo da Tarefa", "Francisco Rafael", DateTime.Now);
+        private readonly TodoHandler _handler = new TodoHandler(new FakeTodoRepository());
+        private GenericCommandresult _result = new GenericCommandresult();
+
+        public CreateTodoHandlerTests()
+        {
+
+        }
+
         [TestMethod]
         public void Dado_comando_invalido_deve_interromper_a_execucao()
         {
-            var command = new CreateTodoCommand("", "", DateTime.Now);
-            var handler = new TodoHandler(new FakeTodoRepository());
-            Assert.Fail();
+            _result = (GenericCommandresult)_handler.Handle(_invalidCommand);
+            Assert.AreEqual(_result.Success, false);
         }
 
         [TestMethod]
         public void Dado_comando_valido_deve_criar_a_tarefa()
         {
-            Assert.Fail();
+            _result = (GenericCommandresult)_handler.Handle(_validCommand);
+            Assert.AreEqual(_result.Success, true);
         }
     }
 }
